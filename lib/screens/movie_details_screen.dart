3242816c1950820db19be4ef10644508b2a07918
Flavio Santana <fvvsantana@gmail.com>
2020-10-02 13:45:40 -0300

    Fix typo

--- lib/screens/movie_details_screen.dart
+++ lib/screens/movie_details_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MoviesDetailsScreen extends StatelessWidget {
+class MovieDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
     appBar: AppBar(title: const Text('Movie Advisor')),
