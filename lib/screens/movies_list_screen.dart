ccc310666340bc118138f3424d24c5854cc94645
Flavio Santana <fvvsantana@gmail.com>
2020-10-02 10:49:23 -0300

    Fetch data from api using Dio and FutureBuilder

--- lib/screens/movies_list_screen.dart
+++ lib/screens/movies_list_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:movie_advisor/utils/links.dart';
 
 class MoviesListScreen extends StatefulWidget {
   @override
@@ -6,16 +10,31 @@ class MoviesListScreen extends StatefulWidget {
 }
 
 class _MoviesListScreenState extends State<MoviesListScreen> {
+  Future<Response> moviesFuture;
 
   @override
   void initState() {
     super.initState();
+    moviesFuture = Dio().get(Links.moviesListUrl);
   }
 
   @override
-  Widget build(BuildContext context) => Container(
-        child: const Center(
-          child: Text('hello'),
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text('Movie Advisor')),
+        body: FutureBuilder(
+          future: moviesFuture,
+          builder: (_, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              final response = snapshot.data;
+              return Center(
+                child: Text(response.data.toString()),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
       );
 }
