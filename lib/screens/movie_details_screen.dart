54f9d790bad03a0fe8979152149db1af6274ac41
Flavio Santana <fvvsantana@gmail.com>
2020-10-02 10:07:03 -0300

    Add screen files

---
+++ lib/screens/movie_details_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MoviesDetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+    child: const Center(
+      child: Text('hello'),
+    ),
+  );
+}
---
+++ lib/screens/movies_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MoviesListScreen extends StatefulWidget {
+  @override
+  _MoviesListScreenState createState() => _MoviesListScreenState();
+}
+
+class _MoviesListScreenState extends State<MoviesListScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: const Center(
+          child: Text('hello'),
+        ),
+      );
+}
