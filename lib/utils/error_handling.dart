3b78c96a44e92da2c2dee6314ee540ccb44d26e7
Flavio Santana <fvvsantana@gmail.com>
2020-10-06 13:07:34 -0300

    Implement error handling utility

---
+++ lib/utils/error_handling.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ErrorHandling {
+  static void showErrorDialog(
+      bool isServerError, BuildContext context, Route route) {
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(isServerError ? 'Server error' : 'Connection error'),
+        content: Text(isServerError
+            ? 'Server error'
+            : 'Server not reachable, please make sure you have '
+                'internet connection.'),
+        actions: [
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+              //Navigator.of(context).pushReplacement(
+              //MaterialPageRoute(builder: (_) => MoviesListScreen()));
+              Navigator.of(context).pushReplacement(route);
+            },
+            child: const Text('Try again'),
+          ),
+        ],
+      ),
+    );
+  }
+}
