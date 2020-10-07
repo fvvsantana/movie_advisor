e9410822d291d0552cfe805c87216b98bba8e700
Flavio Santana <fvvsantana@gmail.com>
2020-10-07 14:38:12 -0300

    Improve some things in the ImageFromNetwork class

--- lib/widgets/image_from_network.dart
+++ lib/widgets/image_from_network.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ImageFromNetwork extends StatelessWidget {
-  const ImageFromNetwork({this.imageUrl, this.fit = BoxFit.contain});
+  const ImageFromNetwork({@required this.imageUrl, this.fit = BoxFit.contain})
+      : assert(imageUrl != null);
 
   final String imageUrl;
   final BoxFit fit;
@@ -9,23 +10,19 @@ class ImageFromNetwork extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Image.network(
         imageUrl,
-        loadingBuilder: (_, child, loadingProgress) {
-          if (loadingProgress == null) {
-            return child;
-          }
-          return Center(
-            child: CircularProgressIndicator(
-              value: loadingProgress.expectedTotalBytes != null
-                  ? loadingProgress.cumulativeBytesLoaded /
-                      loadingProgress.expectedTotalBytes
-                  : null,
-            ),
-          );
-        },
+        loadingBuilder: (_, child, loadingProgress) => loadingProgress == null
+            ? child
+            : Center(
+                child: CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          loadingProgress.expectedTotalBytes
+                      : null,
+                ),
+              ),
         fit: fit,
         errorBuilder: (_, __, ___) => Image.asset(
           'assets/images/no_image_100.png',
-    ),
-
-  );
+        ),
+      );
 }
