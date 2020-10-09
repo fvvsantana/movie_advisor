5f283dd6c9c43acb72ccad3869183180e4e1e338
Flavio Santana <fvvsantana@gmail.com>
2020-10-09 14:54:40 -0300

    Implement custom exceptions

--- lib/utils/error_handling.dart
+++ lib/utils/error_handling.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 
-void showErrorDialog({
-  @required BuildContext context,
-  @required bool isServerError,
-  @required void Function() onTryAgainTap,
-}) {
-  showDialog(
-    context: context,
-    builder: (_) => AlertDialog(
-      title: Text(isServerError ? 'Server error' : 'Connection error'),
-      content: Text(isServerError
-          ? 'Server error'
-          : 'Server not reachable, please make sure you have '
-              'internet connection.'),
-      actions: [
-        FlatButton(
-          onPressed: onTryAgainTap,
-          child: const Text('Try again'),
-        ),
-      ],
-    ),
-  );
+class CustomError implements Exception {
+  const CustomError({@required this.message}) : assert(message != null);
+  final String message;
+
+  @override
+  String toString() => message;
 }
+
+abstract class NetworkError extends CustomError {
+
+  const NetworkError({@required message})
+      : assert(message != null),
+        super(message: message);
+}
+
+class NoInternetError extends NetworkError {
+  const NoInternetError() : super(
+      message: 'Make sure you have internet connection or check your DNS '
+          'settings.');
+}
+
+class ServerResponseError extends NetworkError {
+  const ServerResponseError() : super(message: 'Server error.');
+}
+
+String generateTitle(CustomError error) {
+  try {
+    throw error;
+  } on NoInternetError {
+    return 'Connection Error';
+  } on ServerResponseError{
+    return 'Server error';
+  } catch(_){
+    return 'Error';
+  }
+}
+
