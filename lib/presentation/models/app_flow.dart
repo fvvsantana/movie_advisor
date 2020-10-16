8f5157058e529d2b60b0d1dda8ec78e41dc578f2
Flavio Santana <fvvsantana@gmail.com>
2020-10-16 14:56:18 -0300

    Remove mainColor attribute from AppFlow

--- lib/presentation/models/app_flow.dart
+++ lib/presentation/models/app_flow.dart
@@ -4,16 +4,13 @@ import 'package:flutter/widgets.dart';
 class AppFlow {
   const AppFlow({
     @required this.title,
-    @required this.mainColor,
     @required this.iconData,
     @required this.navigatorKey,
   })  : assert(title != null),
-        assert(mainColor != null),
         assert(iconData != null),
         assert(navigatorKey != null);
 
   final String title;
-  final Color mainColor;
   final IconData iconData;
   final GlobalKey<NavigatorState> navigatorKey;
 }
