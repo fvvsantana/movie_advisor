51b2cd3434a3d31d3582c5b5870bdba2299bba4b
Flavio Santana <fvvsantana@gmail.com>
2020-10-16 19:40:11 -0300

    Implement AdaptiveBottomNavigationScaffold and add it to the home screen

---
+++ lib/presentation/adaptive/bottom_navigation/adaptive_bottom_navigation_scaffold.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+
+import 'package:movie_advisor/presentation/adaptive/bottom_navigation/bottom_navigation_tab.dart';
+import 'package:movie_advisor/presentation/adaptive/bottom_navigation/cupertino_bottom_navigation_scaffold.dart';
+import 'package:movie_advisor/presentation/adaptive/bottom_navigation/material_bottom_navigation_scaffold.dart';
+
+class AdaptiveBottomNavigationScaffold extends StatefulWidget {
+  const AdaptiveBottomNavigationScaffold({
+    @required this.navigationBarTabs,
+    Key key,
+  })  : assert(navigationBarTabs != null),
+        super(key: key);
+
+  final List<BottomNavigationTab> navigationBarTabs;
+
+  @override
+  _AdaptiveBottomNavigationScaffoldState createState() =>
+      _AdaptiveBottomNavigationScaffoldState();
+}
+
+class _AdaptiveBottomNavigationScaffoldState
+    extends State<AdaptiveBottomNavigationScaffold> {
+  int _currentlySelectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) => WillPopScope(
+        onWillPop: () async => !await widget
+            .navigationBarTabs[_currentlySelectedIndex]
+            .navigatorKey
+            .currentState
+            .maybePop(),
+        child: Platform.isAndroid
+            ? CupertinoBottomNavigationScaffold(
+                navigationBarTabs: widget.navigationBarTabs,
+                onItemSelected: onTabSelected,
+                selectedIndex: _currentlySelectedIndex,
+              )
+            : MaterialBottomNavigationScaffold(
+                navigationBarTabs: widget.navigationBarTabs,
+                onItemSelected: onTabSelected,
+                selectedIndex: _currentlySelectedIndex,
+              ),
+      );
+
+  void onTabSelected(int newIndex) {
+    if (_currentlySelectedIndex == newIndex) {
+      widget.navigationBarTabs[newIndex].navigatorKey.currentState
+          .popUntil((route) => route.isFirst);
+    }
+
+    setState(() {
+      _currentlySelectedIndex = newIndex;
+    });
+  }
+}
--- lib/presentation/scenes/home_screen/home_screen.dart
+++ lib/presentation/scenes/home_screen/home_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_advisor/presentation/adaptive/bottom_navigation/adaptive_bottom_navigation_scaffold.dart';
 import 'package:movie_advisor/presentation/adaptive/bottom_navigation/app_flow.dart';
+import 'package:movie_advisor/presentation/adaptive/bottom_navigation/bottom_navigation_tab.dart';
 import 'package:movie_advisor/presentation/scenes/movies_list/movies_list_page.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _currentBarIndex = 0;
+  //int _currentBarIndex = 0;
   final List<AppFlow> _appFlows = [
     AppFlow(
       name: 'Movies',
@@ -26,62 +26,18 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
 
   @override
-  Widget build(BuildContext context) {
-    final currentFlow = _appFlows[_currentBarIndex];
-    return WillPopScope(
-      onWillPop: () async =>
-      !await currentFlow.navigatorKey.currentState.maybePop(),
-      child: Scaffold(
-        body: IndexedStack(
-          index: _currentBarIndex,
-          children: _appFlows
-              .map(
-                (flow) =>
-                Navigator(
-                  key: flow.navigatorKey,
-                  onGenerateRoute: (_) =>
-                      _buildAdaptivePageRoute(
-                        builder: (_) => MoviesListPage(),
-                      ),
+  Widget build(BuildContext context) => AdaptiveBottomNavigationScaffold(
+        navigationBarTabs: _appFlows
+            .map(
+              (flow) => BottomNavigationTab(
+                bottomNavigationBarItem: BottomNavigationBarItem(
+                  label: flow.name,
+                  icon: Icon(flow.iconData),
                 ),
-          )
-              .toList(),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentBarIndex,
-          items: _appFlows
-              .map(
-                (appFlow) =>
-                BottomNavigationBarItem(
-                  label: appFlow.name,
-                  icon: Icon(appFlow.iconData),
-                ),
-          )
-              .toList(),
-          onTap: (index) {
-            if (index != _currentBarIndex) {
-              setState(() {
-                _currentBarIndex = index;
-              });
-            } else {
-              currentFlow.navigatorKey.currentState
-                  .popUntil((route) => route.isFirst);
-            }
-          },
-        ),
-      ),
-    );
-  }
-
-  PageRoute<T> _buildAdaptivePageRoute<T>(
-      {@required WidgetBuilder builder, RouteSettings settings}) =>
-      Platform.isAndroid
-          ? MaterialPageRoute(
-        builder: builder,
-        settings: settings,
-      )
-          : CupertinoPageRoute(
-        builder: builder,
-        settings: settings,
+                navigatorKey: flow.navigatorKey,
+                initialPageBuilder: (_) => MoviesListPage(),
+              ),
+            )
+            .toList(),
       );
 }
