5198428101a591c395b9036c02ad9657b62ebd73
Flavio Santana <fvvsantana@gmail.com>
2020-10-22 18:08:21 -0300

    Set up intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'package:movie_advisor/presentation/scenes/movies_list/movies_list_page.dart';
+import 'package:movie_advisor/generated/l10n.dart';
 
 void main() {
   runApp(
@@ -12,5 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         title: 'Movie Advisor',
         home: MoviesListPage(),
+        localizationsDelegates: [
+          // 1
+          S.delegate,
+          // 2
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
       );
 }
