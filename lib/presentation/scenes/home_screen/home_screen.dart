cc8c60fa759e396b85e0202503a103d4bdf2b4e6
Flavio Santana <fvvsantana@gmail.com>
2020-10-23 16:30:55 -0300

    Translate every title, bottom navigation bar has a problem though
    The bottom navigation bar doesn't change the content of its widgets when the language changes during the app exectution.

--- lib/presentation/scenes/home_screen/home_screen.dart
+++ lib/presentation/scenes/home_screen/home_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:movie_advisor/presentation/adaptive/bottom_navigation/adaptive_bottom_navigation_scaffold.dart';
 import 'package:movie_advisor/presentation/adaptive/bottom_navigation/app_flow.dart';
 import 'package:movie_advisor/presentation/adaptive/bottom_navigation/bottom_navigation_tab.dart';
 import 'package:movie_advisor/presentation/scenes/movies_list/movies_list_page.dart';
+
+import 'package:movie_advisor/generated/l10n.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,18 +14,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<AppFlow> _appFlows = [
-    AppFlow(
-      name: 'Movies',
-      iconData: Icons.ondemand_video,
-      navigatorKey: GlobalKey<NavigatorState>(),
-    ),
-    AppFlow(
-      name: 'Favorites',
-      iconData: Icons.star_border,
-      navigatorKey: GlobalKey<NavigatorState>(),
-    ),
-  ];
+  List<AppFlow> _appFlows;
+
+  Locale _userLocale;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final newLocale = Localizations.localeOf(context);
+    if (newLocale != _userLocale) {
+      _userLocale = newLocale;
+
+      _appFlows = [
+        AppFlow(
+          name: S.of(context).bottomNavigationMoviesTitle,
+          iconData: Icons.ondemand_video,
+          navigatorKey: GlobalKey<NavigatorState>(),
+        ),
+        AppFlow(
+          name: S.of(context).bottomNavigationFavoritesTitle,
+          iconData: Icons.star_border,
+          navigatorKey: GlobalKey<NavigatorState>(),
+        ),
+      ];
+    }
+  }
 
   @override
   Widget build(BuildContext context) => AdaptiveBottomNavigationScaffold(
