8ea70ba04b55f64cc2ec22a7ddbcae1083fc3478
Flavio Santana <fvvsantana@gmail.com>
2020-11-09 11:59:27 -0300

    Create AsyncSnapshotResponseView

---
+++ lib/presentation/common/async_snapshot_response_view.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:movie_advisor/presentation/common/centered_progress_indicator.dart';
+
+/// Chooses between a [CenteredProgressIndicator], an error widget or a
+/// content widget by matching the snapshot's data with the provided generic
+/// types.
+class AsyncSnapshotResponseView<Loading, Error, Success>
+    extends StatelessWidget {
+  AsyncSnapshotResponseView({
+    @required this.snapshot,
+    @required this.successWidgetBuilder,
+    @required this.errorWidgetBuilder,
+    Key key,
+  })  : assert(snapshot != null),
+        assert(successWidgetBuilder != null),
+        assert(errorWidgetBuilder != null),
+        assert(Loading != dynamic),
+        assert(Error != dynamic),
+        assert(Success != dynamic),
+        super(key: key);
+
+  final AsyncSnapshot snapshot;
+  final Widget Function(BuildContext context, Success success)
+      successWidgetBuilder;
+  final Widget Function(BuildContext context, Error error) errorWidgetBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    final snapshotData = snapshot.data;
+    if (snapshotData == null || snapshotData is Loading) {
+      return CenteredProgressIndicator();
+    }
+
+    if (snapshotData is Error) {
+      return errorWidgetBuilder(context, snapshotData);
+    }
+
+    if (snapshotData is Success) {
+      return successWidgetBuilder(context, snapshotData);
+    }
+
+    throw UnknownStateTypeException();
+  }
+}
+
+class UnknownStateTypeException implements Exception {}
