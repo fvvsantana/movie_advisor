e3fd6aeb751d6ea00d10c790338090d634b6be9e
Flavio Santana <fvvsantana@gmail.com>
2020-11-09 17:16:58 -0300

    Fix formatting of RouteNameBuilder

--- lib/presentation/route_name_builder.dart
+++ lib/presentation/route_name_builder.dart
@@ -1,8 +1,9 @@
-class RouteNameBuilder{
+class RouteNameBuilder {
   static const String home = '/';
   static const String moviesResource = 'movies';
   static const String moviesPathParameterId = 'id';
 
   static String moviesList() => moviesResource;
+
   static String movieById(int id) => '$moviesResource/$id';
 }
