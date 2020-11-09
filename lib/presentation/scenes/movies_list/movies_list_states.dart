1477f0e676e14afef319307d43600cd0ec9093a4
Flavio Santana <fvvsantana@gmail.com>
2020-11-09 12:13:22 -0300

    Add MoviesListResponseState and its implementations

---
+++ lib/presentation/scenes/movies_list/movies_list_states.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:movie_advisor/common/errors.dart';
+import 'package:movie_advisor/data/models/movie_summary_model.dart';
+
+abstract class MoviesListResponseState {}
+
+class Loading implements MoviesListResponseState{}
+
+class Error implements MoviesListResponseState {
+  const Error({@required this.error}) : assert(error != null);
+  final CustomError error;
+}
+
+class Success implements MoviesListResponseState {
+  const Success({@required this.list}) : assert(list != null);
+  final List<MovieSummaryModel> list;
+}
