dec0eca52f3b45cf9c78a6e2d3db2a51888ab0b0
Flavio <fvvsantana@gmail.com>
2020-11-12 16:44:53 -0300

    Register adapter in the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_advisor/presentation/route_name_builder.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hive/hive.dart';
+import 'package:movie_advisor/data/cache/models/movie_summary_cm.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'package:movie_advisor/presentation/route_name_builder.dart';
 import 'package:movie_advisor/generated/l10n.dart';
 import 'package:movie_advisor/presentation/scenes/home_screen/home_screen.dart';
 import 'package:movie_advisor/presentation/scenes/movie_details/movie_details_page.dart';
 import 'package:movie_advisor/presentation/scenes/movies_list/movies_list_page.dart';
 
-void main() {
+Future<void> main() async {
   // Define routes
   FluroRouter.appRouter
     ..define(
@@ -29,6 +33,14 @@ void main() {
           id: int.parse(params[RouteNameBuilder.moviesPathParameterId][0]),
         ),
       ),
+    );
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Hive
+    ..init((await getApplicationDocumentsDirectory()).path)
+    ..registerAdapter<MovieSummaryCM>(
+      MovieSummaryCMAdapter(),
     );
 
   runApp(
