0da70baec3c6507ae839c716d18f06919aceae5b
Flavio <fvvsantana@gmail.com>
2020-11-12 16:23:56 -0300

    Add hive annotations to MovieSummaryCM

--- lib/data/cache/models/movie_summary_cm.dart
+++ lib/data/cache/models/movie_summary_cm.dart
@@ -1,3 +1,24 @@
-class MovieSummaryCM{
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
 
+part 'movie_summary_cm.g.dart';
+
+
+@HiveType(typeId: 0)
+class MovieSummaryCM{
+  const MovieSummaryCM({
+    @required this.id,
+    @required this.title,
+    @required this.imageUrl,
+  })  : assert(id != null),
+        assert(title != null),
+        assert(imageUrl != null);
+
+
+  @HiveField(0)
+  final int id;
+  @HiveField(1)
+  final String title;
+  @HiveField(2)
+  final String imageUrl;
 }
