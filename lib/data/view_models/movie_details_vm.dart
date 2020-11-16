d583f82cf98bcdc77388f33290d3a6bb499162a1
Flavio <fvvsantana@gmail.com>
2020-11-16 13:41:34 -0300

    Rename models to view models

--- lib/data/view_models/movie_details_vm.dart
+++ lib/data/view_models/movie_details_vm.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 
-class MovieDetailsModel {
-  const MovieDetailsModel({
+class MovieDetailsVM {
+  const MovieDetailsVM({
     @required this.id,
     @required this.title,
     @required this.imageUrl,
--- lib/data/view_models/movie_summary_vm.dart
+++ lib/data/view_models/movie_summary_vm.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 
-class MovieSummaryModel {
-  const MovieSummaryModel({
+class MovieSummaryVM {
+  const MovieSummaryVM({
     @required this.id,
     @required this.title,
     @required this.imageUrl,
