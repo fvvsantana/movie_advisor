43aaee9ddac5926d03b074001e2a37530ce12e56
Flavio <fvvsantana@gmail.com>
2020-11-16 16:40:31 -0300

    Implement getMoviesList on repository

--- lib/data/repository.dart
+++ lib/data/repository.dart
@@ -1,18 +1,46 @@
+import 'package:movie_advisor/data/cache/cache_data_source.dart';
 import 'package:movie_advisor/data/remote/remote_data_source.dart';
+import 'package:movie_advisor/model/movie_summary.dart';
+import 'package:movie_advisor/data/mappers/remote_to_cache.dart';
+import 'package:movie_advisor/data/mappers/cache_to_domain.dart';
 
-class Repository{
-  final _rds = RemoteDataSource();
+class Repository {
+  final _remoteDS = RemoteDataSource();
+  final _cacheDS = CacheDataSource();
 
-  /*
-  Future<List<MovieSummaryModel>> getMoviesList() async{
+  Future<List<MovieSummary>> getMoviesList() async {
     // Make request
     try {
-      final moviesList = await _rds.getMoviesList();
-      moviesList.map((rm) => rm.toModel()).toList();
-    }catch(error){
+      // Fetch movies list from network
+      final moviesListR = await _remoteDS.getMoviesList();
 
+      // Remote to cache
+      final moviesListC = moviesListR
+          .map(
+            (remoteModel) => remoteModel.toCache(),
+          )
+          .toList();
+      // Update cache
+      await _cacheDS.upsertMoviesList(moviesListC);
+
+      // Cache to domain
+      return moviesListC
+          .map(
+            (cacheModel) => cacheModel.toDomain(),
+          )
+          .toList();
+    } catch (error) {
+      final moviesListC = await _cacheDS.getMoviesList();
+      // Cache hit check
+      if (moviesListC != null) {
+        return moviesListC
+            .map(
+              (remoteModel) => remoteModel.toDomain(),
+            )
+            .toList();
+      }
+
+      rethrow;
     }
   }
-
-   */
 }
