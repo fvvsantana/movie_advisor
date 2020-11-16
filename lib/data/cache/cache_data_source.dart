12bc89180868c01bdcb39c03c37727e444a1ad15
Flavio <fvvsantana@gmail.com>
2020-11-16 12:51:44 -0300

    Implement CacheDataSource for MoviesList

--- lib/data/cache/cache_data_source.dart
+++ lib/data/cache/cache_data_source.dart
@@ -1,3 +1,18 @@
-class CacheDataSource{
+import 'package:hive/hive.dart';
 
+import 'models/movie_summary_cm.dart';
+
+class CacheDataSource {
+  static const _moviesListBoxName = 'moviesListBox';
+  static const _moviesListKeyName = 'moviesListKey';
+
+  Future<void> upsertMoviesList(List<MovieSummaryCM> moviesList) =>
+      Hive.openBox<List<MovieSummaryCM>>(_moviesListBoxName).then(
+        (box) => box.put(_moviesListKeyName, moviesList),
+      );
+
+  Future<List<MovieSummaryCM>> getMoviesList() =>
+      Hive.openBox<List<MovieSummaryCM>>(_moviesListBoxName).then(
+        (box) => box.get(_moviesListKeyName),
+      );
 }
