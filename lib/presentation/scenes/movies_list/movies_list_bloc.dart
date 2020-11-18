1b949ea44fedc517028ebeab4b7d6b1191f7c742
Flavio <fvvsantana@gmail.com>
2020-11-18 13:45:50 -0300

    Remove unused import

--- lib/presentation/scenes/movies_list/movies_list_bloc.dart
+++ lib/presentation/scenes/movies_list/movies_list_bloc.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:rxdart/rxdart.dart';
 
-import 'package:movie_advisor/common/errors.dart';
 import 'package:movie_advisor/data/remote/movie_remote_data_source.dart';
 import 'package:movie_advisor/presentation/scenes/movies_list/movies_list_states.dart';
 
