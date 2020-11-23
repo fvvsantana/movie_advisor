8c327578373cf60cd18fddb16c096c99b066996b
Flavio <fvvsantana@gmail.com>
2020-11-23 13:11:32 -0300

    Add focus detector to FavoriteMoviesPage

--- lib/presentation/scenes/favorite_movies/favorite_movies_page.dart
+++ lib/presentation/scenes/favorite_movies/favorite_movies_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:focus_detector/focus_detector.dart';
 
 import 'package:movie_advisor/generated/l10n.dart';
 import 'package:movie_advisor/presentation/common/async_snapshot_response_view.dart';
@@ -16,6 +17,7 @@ class FavoriteMoviesPage extends StatefulWidget {
 
 class _FavoriteMoviesPageState extends State<FavoriteMoviesPage> {
   final _bloc = FavoriteMoviesBloc();
+  final _focusDetectorKey = UniqueKey();
 
   void _pushMovieDetails(int movieId) {
     Navigator.of(context).pushNamed(
@@ -28,25 +30,29 @@ class _FavoriteMoviesPageState extends State<FavoriteMoviesPage> {
         appBar: AppBar(
           title: Text(S.of(context).favoriteMoviesAppBarTitle),
         ),
-        body: StreamBuilder<FavoriteMoviesResponseState>(
-          stream: _bloc.onNewState,
-          builder: (context, snapshot) =>
-              AsyncSnapshotResponseView<Loading, Error, Success>(
-                  snapshot: snapshot,
-                  errorWidgetBuilder: (context, errorState) => ErrorEmptyState(
-                        error: errorState.error,
-                        onTryAgainTap: () => _bloc.onTryAgain.add(null),
-                      ),
-                  successWidgetBuilder: (context, successState) {
-                    final favoriteMovies = successState.favoriteMovies;
-                    if (favoriteMovies.isEmpty) {
-                      return FavoriteMoviesEmptyState();
-                    }
-                    return MoviesList(
-                      movies: successState.favoriteMovies,
-                      onMovieTap: _pushMovieDetails,
-                    );
-                  }),
+        body: FocusDetector(
+          onFocusGained: () => _bloc.onTryAgain.add(null),
+          key: _focusDetectorKey,
+          child: StreamBuilder<FavoriteMoviesResponseState>(
+            stream: _bloc.onNewState,
+            builder: (context, snapshot) =>
+                AsyncSnapshotResponseView<Loading, Error, Success>(
+                    snapshot: snapshot,
+                    errorWidgetBuilder: (context, errorState) => ErrorEmptyState(
+                          error: errorState.error,
+                          onTryAgainTap: () => _bloc.onTryAgain.add(null),
+                        ),
+                    successWidgetBuilder: (context, successState) {
+                      final favoriteMovies = successState.favoriteMovies;
+                      if (favoriteMovies.isEmpty) {
+                        return FavoriteMoviesEmptyState();
+                      }
+                      return MoviesList(
+                        movies: successState.favoriteMovies,
+                        onMovieTap: _pushMovieDetails,
+                      );
+                    }),
+          ),
         ),
       );
 }
