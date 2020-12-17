eb19aed324d8e62ba723646346b43e7565534e04
Flavio <fvvsantana@gmail.com>
2020-12-17 13:56:31 -0300

    Make a provider for FluroRouter and do the setup inside of build methods

--- lib/common/global_providers.dart
+++ lib/common/global_providers.dart
@@ -1,5 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter/widgets.dart';
 import 'package:hive/hive.dart';
+import 'package:movie_advisor/data/cache/models/movie_details_cm.dart';
+import 'package:movie_advisor/data/cache/models/movie_summary_cm.dart';
+import 'package:movie_advisor/presentation/routing.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
@@ -8,9 +14,16 @@ import 'package:movie_advisor/data/remote/movie_remote_data_source.dart';
 import 'package:movie_advisor/data/repository.dart';
 
 final List<SingleChildWidget> globalProviders = [
+  ..._routingProviders,
   ..._rdsProviders,
   ..._cdsProviders,
   ..._repositoryProviders,
+];
+
+final List<SingleChildWidget> _routingProviders = [
+  Provider<FluroRouter>(
+    create: (_) => _buildFluroRouter(),
+  ),
 ];
 
 final List<SingleChildWidget> _rdsProviders = [
@@ -28,7 +41,7 @@ final List<SingleChildWidget> _rdsProviders = [
 
 final List<SingleChildWidget> _cdsProviders = [
   Provider<HiveInterface>(
-    create: (_) => Hive,
+    create: (_) => _buildHive(),
   ),
   ProxyProvider<HiveInterface, MovieCacheDataSource>(
     update: (_, hive, cds) =>
@@ -45,3 +58,25 @@ final List<SingleChildWidget> _repositoryProviders = [
         repository ?? Repository(movieRDS: rds, movieCDS: cds),
   ),
 ];
+
+FluroRouter _buildFluroRouter() {
+  defineRoutes();
+  return FluroRouter.appRouter;
+}
+
+HiveInterface _buildHive() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  getApplicationDocumentsDirectory().then((dir) {
+    Hive
+      ..init(dir.path)
+      ..registerAdapter<MovieSummaryCM>(
+        MovieSummaryCMAdapter(),
+      )
+      ..registerAdapter<MovieDetailsCM>(
+        MovieDetailsCMAdapter(),
+      );
+  });
+
+  return Hive;
+}
