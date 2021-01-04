ae1d4e975ea109a00eecb894693d0075e7f1c505
Flavio Santana <fvvsantana@gmail.com>
2021-01-04 14:55:55 -0300

    Create SetFavoriteMovieUC

---
+++ domain/lib/use_cases/set_favorite_movie_uc.dart
@@ -0,0 +1,32 @@
+import 'package:meta/meta.dart';
+
+import 'package:domain/gateways/movie_repository.dart';
+import 'package:domain/gateways/error_logger.dart';
+import 'package:domain/models/movie_details.dart';
+import 'package:domain/use_cases/use_case.dart';
+
+class SetFavoriteMovieUC extends UseCase<SetFavoriteMovieUCParams, void> {
+  const SetFavoriteMovieUC({
+    @required this.repository,
+    @required ErrorLoggerGateway logger,
+  })  : assert(repository != null),
+        assert(logger != null),
+        super(logger: logger);
+
+  final MovieRepositoryGateway repository;
+
+  @override
+  Future<MovieDetails> getRawFuture(
+          {@required SetFavoriteMovieUCParams params}) =>
+      repository.setFavoriteMovie(params.movieId, params.isFavorite);
+}
+
+class SetFavoriteMovieUCParams {
+  const SetFavoriteMovieUCParams({
+    @required this.movieId,
+    @required this.isFavorite,
+  })  : assert(movieId != null),
+        assert(isFavorite != null);
+  final int movieId;
+  final bool isFavorite;
+}
